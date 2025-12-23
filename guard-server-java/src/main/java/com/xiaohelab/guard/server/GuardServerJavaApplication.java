88bf621776fc9a1363643f04799306fc92525c7d
Xiaohe <heshaobo0222@outlook.com>
2025-12-23 22:28:40 +0800

    Init：Project structure setup

---
+++ guard-server-java/src/main/java/com/xiaohelab/guard/server/GuardServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohelab.guard.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuardServerJavaApplication.class, args);
+    }
+
+}
