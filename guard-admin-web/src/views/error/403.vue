b6ea118283a336fef21cda14364b9a59cfaef543
Xiaohe <heshaobo0222@outlook.com>
2026-04-22 23:23:39 +0800

    删除了管理端的全部代码，全量重构

--- guard-admin-web/src/views/error/403.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="padding: 80px; text-align: center">
-    <h2>403 - 无访问权限</h2>
-    <p>您没有权限访问该页面，请联系管理员。</p>
-    <a-button type="primary" @click="$router.push('/admin/dashboard')">返回首页</a-button>
-  </div>
-</template>
