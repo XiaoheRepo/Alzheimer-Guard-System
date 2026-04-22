3a4e041a6361a83aa307b71e1c08fe14a8419ecd
Xiaohe <heshaobo0222@outlook.com>
2026-04-22 23:31:45 +0800

    feat: 更新环境配置，调整API地址，新增WebSocket支持，删除不必要的常量文件

--- guard-admin-web/vite.config.ts
+++ guard-admin-web/vite.config.ts
@@ -6,13 +6,27 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    sourcemap: 'hidden',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-vue': ['vue', 'vue-router', 'pinia'],
+          'vendor-antdv': ['ant-design-vue'],
+          'vendor-echarts': ['echarts', 'vue-echarts'],
+          'vendor-i18n': ['vue-i18n', 'dayjs'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
   },
 })
